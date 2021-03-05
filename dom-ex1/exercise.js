7d9d0dcbf2fbdee6c6930c55ee22350ca7c3a47a
hassanwasswa <assanwasswa@gmail.com>
2021-03-05 18:01:41 +0100

    dom-exe1 done

--- dom-ex1/exercise.js
+++ dom-ex1/exercise.js
@@ -1,6 +1,10 @@
+var button = document.querySelector('button');
 
-function textGenerator() {
-    var myText = document.getElementById('input').value;
-    document.getElementById('paragraph').innerHTML = myText
-    
-  };
+var input = document.querySelector('input');
+
+function clickButton(){
+    var pTag = document.createElement('p');
+    pTag.appendChild(document.createTextNode(input.value));
+    document.body.appendChild(pTag);
+}
+button.addEventListener('click', clickButton)
